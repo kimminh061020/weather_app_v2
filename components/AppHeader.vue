5a7d4e7ce38d504ce491e0b0f800ccaf236905a5
Mark <kimminh061020@gmail.com>
2022-03-07 17:54:39 +0700

    Weather App V2

---
+++ components/AppHeader.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="nav-wrap">
+       <img class="" src="../assets/weather.png" height="70px">
+  <b-nav align="center">
+     
+    
+    <b-nav-item >
+        <router-link to="/" id="nav-bar-home">Home</router-link>
+    </b-nav-item>
+    <b-nav-item >
+        <router-link to="/about" id="nav-bar-about">About</router-link>
+    </b-nav-item>
+  </b-nav>
+  <div id="button_wrap">
+  <b-button pill id="button">Contact</b-button>
+  </div>
+</div>
+</template>
+
+<script>
+import image from "../assets/weather.png"
+
+
+
+export default {
+name: 'AppHeader',
+
+}
+</script>
+
+<style scoped>
+.nav-wrap {
+display: grid;
+grid-template-columns: 1fr 2fr 1fr;
+background: black;
+padding-bottom: 10px;
+padding-top: 10px;
+}
+
+#nav-bar-home, #nav-bar-about {
+    color: white;
+    font-size: 20px;
+    padding: 10px;
+
+}
+
+img {
+    padding-left: 30px;
+}
+
+.justify-content-center {
+    justify-content: center !important;
+    align-content: center;
+}
+
+#button_wrap {
+    color: black;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+}
+#button {
+    background: white;
+    color: black;
+    font-weight: 600;
+    margin-right: 10%;
+    width: 200px
+}
+
+@media only screen and (max-width: 980px) {
+    #nav-bar-home, #nav-bar-about {
+    font-size: 15px;
+    padding: 0px;
+}
+#button {
+    background: white;
+    width: 100px
+}
+}
+</style>
---
+++ pages/about.vue
@@ -0,0 +1,67 @@
+<template>
+<div>
+    <AppHeader />
+    
+<div id="about-wrapper">
+    <h1> Made By Mark Nguyen</h1>
+<div class="carousel-inner" role="listbox" style=" width:1024px; height: 480px !important; text-align: center">
+  <b-carousel
+    id="carousel-fade"
+    style="text-shadow: 0px 0px 2px #000"
+    fade
+    indicators
+    img-width="1024"
+    img-height="480"
+  >
+    <b-carousel-slide
+      caption="Awesome Weathenur App"
+      text="This is an app to check weather based on city names"
+      img-src="../assets/rodion-kutsaev-PEm_sLmJT-w-unspl.jpg"
+    ></b-carousel-slide>
+    <b-carousel-slide
+      caption="Awesome Weather App"
+      text="This is an app to check weather based on city names"
+      img-src="../assets/jeremy-thomas-E0AHdsENmDg-unspla.jpg"
+    ></b-carousel-slide>
+    <b-carousel-slide
+      caption="Awesome Weather App"
+      text="This is an app to check weather based on city names"
+      img-src="../assets/damian-mccoig-2HqpqSqy0zg-unspla.jpg"
+    ></b-carousel-slide>
+  </b-carousel>
+</div>
+</div>
+</div>
+</template>
+
+<script>
+import AppHeader from '../components/AppHeader.vue'
+export default {
+  components: { AppHeader },
+head() {
+    return {
+        title: 'About The App',
+        meta: [
+            {
+                hid: 'description',
+                name: 'description',
+                content: 'Best app to check weather information',
+            }
+        ]
+    }
+}
+}
+</script>
+
+
+<style scoped>
+#about-wrapper {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+h1 {
+    padding-top: 2%;
+    padding-bottom: 2%;
+}
+</style>
